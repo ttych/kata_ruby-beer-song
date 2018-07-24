2aeb12096cb11c63d80a1e8d9ad529678182b721
ttych <thomas.tych@gmail.com>
2018-07-24 21:52:20 +0200

    [test][refacto] test_the_first_verse

--- ruby/lib/beer_song.rb
+++ ruby/lib/beer_song.rb
@@ -1,7 +1,11 @@
 # BeerSong lyrics as Code
 class BeerSong
-  def verse(_id)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" \
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  VERSE = '%<id>s bottles of beer on the wall,' \
+          " %<id>s bottles of beer.\n" \
+          'Take one down and pass it around,' \
+          " %<next_id>s bottles of beer on the wall.\n".freeze
+
+  def verse(id)
+    format(VERSE, id: id, next_id: id - 1)
   end
 end
--- ruby/test/beer_song_test.rb
+++ ruby/test/beer_song_test.rb
@@ -11,6 +11,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_another_verse
+    skip
     expected = "3 bottles of beer on the wall, 3 bottles of beer.\n" \
       "Take one down and pass it around, 2 bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(3)
