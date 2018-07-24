5850c6fc69be57228c140522bd5a87d07635686f
ttych <thomas.tych@gmail.com>
2018-07-24 22:09:53 +0200

    [test][green] test_verse_2_is_correct

--- ruby/lib/beer_song.rb
+++ ruby/lib/beer_song.rb
@@ -1,11 +1,20 @@
 # BeerSong lyrics as Code
 class BeerSong
-  VERSE = '%<id>s bottles of beer on the wall,' \
-          " %<id>s bottles of beer.\n" \
+  VERSE = '%<id>s of beer on the wall,' \
+          " %<id>s of beer.\n" \
           'Take one down and pass it around,' \
-          " %<next_id>s bottles of beer on the wall.\n".freeze
+          " %<next_id>s of beer on the wall.\n".freeze
+  BOTTLE = 'bottle'.freeze
 
   def verse(id)
-    format(VERSE, id: id, next_id: id - 1)
+    format(VERSE, id: _n_bottle_s(id),
+                  next_id: _n_bottle_s(id - 1))
+  end
+
+  private
+
+  def _n_bottle_s(id)
+    return "#{id} #{BOTTLE}" if id < 2
+    "#{id} #{BOTTLE}s"
   end
 end
