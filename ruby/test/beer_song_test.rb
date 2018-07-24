861056f810c617f95bd89d4ec260de4a7dea08b6
ttych <thomas.tych@gmail.com>
2018-07-24 21:20:16 +0200

    [test] fix quality/reek issue on tests

--- ruby/test/beer_song_test.rb
+++ ruby/test/beer_song_test.rb
@@ -18,21 +18,21 @@ class BeerSongTest < Minitest::Test
     assert_equal expected, BeerSong.new.verse(3)
   end
 
-  def test_verse_2
+  def test_verse_2_is_correct
     skip
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(2)
   end
 
-  def test_verse_1
+  def test_verse_1_is_correct
     skip
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(1)
   end
 
-  def test_verse_0
+  def test_verse_0_is_correct
     skip
     expected = "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
