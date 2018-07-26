d8328998efed581a92e46bfae9b4f037d8d966c5
ttych <thomas.tych@gmail.com>
2018-07-26 13:14:37 +0200

    [test][refacto] all

--- ruby/lib/beer_song.rb
+++ ruby/lib/beer_song.rb
@@ -2,33 +2,18 @@ require 'book_keeping'
 
 # BeerSong lyrics as Code
 class BeerSong
-  VERSE_DEFAULT = '%<id>s of beer on the wall,' \
-                  " %<id>s of beer.\n" \
-                  'Take one down and pass it around,' \
-                  " %<next_id>s of beer on the wall.\n".freeze
-
-  VERSE_LAST = '%<id>s of beer on the wall,' \
-               " %<id>s of beer.\n" \
-               'Take it down and pass it around,' \
-               " no more bottles of beer on the wall.\n".freeze
-
-  VERSE_RESET = 'No more bottles of beer on the wall,' \
-                " no more bottles of beer.\n" \
-                'Go to the store and buy some more,' \
-                " 99 bottles of beer on the wall.\n".freeze
-
+  VERSE = "%<bottles_cap>s of beer on the wall, %<bottles>s of beer.\n" \
+          "%<action>s, %<next_bottles>s of beer on the wall.\n".freeze
+  VERSE_ACTION_EMPTY = 'Go to the store and buy some more'.freeze
+  VERSE_ACTION_TAKE = 'Take %<count>s down and pass it around'.freeze
   BOTTLE = 'bottle'.freeze
 
   def verse(id)
-    verse = if id.zero?
-              VERSE_RESET
-            elsif id == 1
-              VERSE_LAST
-            else
-              VERSE_DEFAULT
-            end
-    format(verse, id: _n_bottle_s(id),
-                  next_id: _n_bottle_s(id - 1))
+    format(VERSE,
+           bottles_cap: _n_bottle_s(id).capitalize,
+           bottles: _n_bottle_s(id),
+           action: _action(id),
+           next_bottles: _n_bottle_s(id - 1))
   end
 
   def verses(id_start, id_end)
@@ -43,8 +28,15 @@ class BeerSong
 
   private
 
-  def _n_bottle_s(id)
-    return "#{id} #{BOTTLE}" if id < 2
-    "#{id} #{BOTTLE}s"
+  def _n_bottle_s(count)
+    count = 99 if count.negative?
+    return "no more #{BOTTLE}s" if count.zero?
+    "#{count} #{BOTTLE}#{'s' if count > 1}"
+  end
+
+  def _action(count)
+    return VERSE_ACTION_EMPTY if count.zero?
+    format(VERSE_ACTION_TAKE,
+           count: count == 1 ? 'it' : 'one')
   end
 end
