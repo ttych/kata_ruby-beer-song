a670c21dd310105e54b575fd2a26070e8f5abfe3
JustXavier <xavier.aris@gmail.com>
2018-08-08 20:43:25 +0200

    round3

--- beer_song.rb
+++ beer_song.rb
@@ -1,13 +1,26 @@
 # BeerSong class
 class BeerSong
   def verse(nb_beer)
-    if nb_beer > 2
+    case nb_beer
+    when 3..100
+
       "#{nb_beer} bottles of beer on the wall, #{nb_beer} bottles of beer.\n" \
       'Take one down and pass it around, ' \
       "#{nb_beer - 1} bottles of beer on the wall.\n"
-    elsif nb_beer == 2
+
+    when 2
       "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" \
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
+  end
+  
+  def verses(nba, nbb)
+    verse(nba) + "\n" + verse(nbb)
   end
 end
--- beer_song_test.rb
+++ beer_song_test.rb
@@ -24,21 +24,19 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_verse_1
-    skip
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(1)
   end
 
   def test_verse_0
-    skip
+ 
     expected = "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(0)
   end
 
   def test_a_couple_verses
-    skip
     expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" \
       "Take one down and pass it around, 98 bottles of beer on the wall.\n" \
       "\n" \
